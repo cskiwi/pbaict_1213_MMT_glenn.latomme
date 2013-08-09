30c12881a7b3498bdd20eca816f452e0435349ce
Glenn Latomme <glenn.latomme@gmail.com>
2013-08-09 16:42:46 +0200

    Commit pt2

---
+++ MMT2/GameAI/RRT/RRTBranch.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace GameAI.RRT {
+    public class RrtBranch {
+        public static int BranchLength = 50;
+
+        public RrtBranch(Point loc, int parentID) {
+            Location = loc;
+            ParentID = parentID;
+        }
+
+        public RrtBranch() {}
+        public Point Location { get; set; }
+        public int ParentID { get; set; }
+
+        public RrtBranch NextBranch(Point goalLoc, int parent) {
+            var newBranch = new RrtBranch();
+            var vector = new Vector(goalLoc.X - Location.X, goalLoc.Y - Location.Y);
+            vector.Normalize();
+            newBranch.Location = new Point(
+                (int) (Location.X + (vector.X * BranchLength)),
+                (int) (Location.Y + (vector.Y * BranchLength)));
+            newBranch.ParentID = parent;
+            return newBranch;
+        }
+    }
+}
